3a5219d4990a55e2abebfefc38f936f774d71aa5
marcolino <marcosolari@gmail.com>
2014-05-16 18:00:18 +0200

    added local-storage dependency

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,19 +4,27 @@ app.controller('MainCtrl', function ($scope, $timeout) {
     $scope.team1 = [];
     $scope.team2 = [];
    
+    $scope.sports = [
+      { 'name': 'Calcio a 5', 'players': 5 },
+      { 'name': 'Calcio a 7', 'players': 7 },
+      { 'name': 'Calcio a 8', 'players': 8 },
+      { 'name': 'Calcio',     'players': 11 },
+      { 'name': 'Rugby',      'players': 15 },
+    ];
+
     $scope.players = [
-      { 'title': 'Frinks',    'drag': true },
-      { 'title': 'Lucio',     'drag': true },
-      { 'title': 'Soletta',   'drag': true },
-      { 'title': 'Paoloalgo', 'drag': true },
-      { 'title': 'Marcotono', 'drag': true },
-      { 'title': 'Attila',    'drag': true },
-      { 'title': 'Puntone',   'drag': true },
-      { 'title': 'Bonnie',    'drag': true },
-      { 'title': 'Remi',      'drag': true },
-      { 'title': 'Grigio',    'drag': true },
-      { 'title': 'Mosso',     'drag': true },
-      { 'title': 'Nordin',    'drag': true }
+      { 'name': 'Frinks',    'drag': true },
+      { 'name': 'Lucio',     'drag': true },
+      { 'name': 'Soletta',   'drag': true },
+      { 'name': 'Paoloalgo', 'drag': true },
+      { 'name': 'Marcotono', 'drag': true },
+      { 'name': 'Attila',    'drag': true },
+      { 'name': 'Puntone',   'drag': true },
+      { 'name': 'Bonnie',    'drag': true },
+      { 'name': 'Remi',      'drag': true },
+      { 'name': 'Grigio',    'drag': true },
+      { 'name': 'Mosso',     'drag': true },
+      { 'name': 'Nordin',    'drag': true }
     ];
 
     // limit items to be dropped in list1
--- app/scripts/controllers/setup.js
+++ app/scripts/controllers/setup.js
@@ -1,6 +1,33 @@
 'use strict';
 
 app.controller('SetupCtrl', function ($scope) {
+
+  $scope.team1 = [];
+  $scope.team2 = [];
+ 
+  $scope.sports = [
+    { 'name': 'Calcio a 5', 'players': 5 },
+    { 'name': 'Calcio a 7', 'players': 7 },
+    { 'name': 'Calcio a 8', 'players': 8 },
+    { 'name': 'Calcio',     'players': 11 },
+    { 'name': 'Rugby',      'players': 15 },
+  ];
+
+  $scope.players = [
+    { 'name': 'Frinks',    'drag': true },
+    { 'name': 'Lucio',     'drag': true },
+    { 'name': 'Soletta',   'drag': true },
+    { 'name': 'Paoloalgo', 'drag': true },
+    { 'name': 'Marcotono', 'drag': true },
+    { 'name': 'Attila',    'drag': true },
+    { 'name': 'Puntone',   'drag': true },
+    { 'name': 'Bonnie',    'drag': true },
+    { 'name': 'Remi',      'drag': true },
+    { 'name': 'Grigio',    'drag': true },
+    { 'name': 'Mosso',     'drag': true },
+    { 'name': 'Nordin',    'drag': true }
+  ];
+
   $scope.colors = [
     {name:'black', shade:'dark'},
     {name:'white', shade:'light'},
